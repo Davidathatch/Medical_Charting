ff1571015d98d028914dfb58d85e5e1cd35a818b
davidathatch1 <60905784+davidathatch1@users.noreply.github.com>
2022-04-20 16:21:11 -0600

    Completed vitals form data collection in JS

--- local/app.js
+++ local/app.js
@@ -192,19 +192,136 @@ function getData() {
     ioData["output-description"] = quickId("output-description");
 
     //IO TOTAL VALUES
-    ioData["io-total-value"] = quickId("io-total-value");
-    ioData["io-total-method"] = quickId("io-total-method");
-    ioData["io-total-time"] = quickId("io-total-time");
-    ioData["io-total-description"] = quickId("io-total-description");
+    ioData["total-io-value"] = quickId("total-io-value");
+    ioData["total-io-method"] = quickId("total-io-method");
+    ioData["total-time-io"] = quickId("total-time-io");
+    ioData["total-io-description"] = quickId("total-io-description");
 
     //ADD DATA TO "postValues"
     postValues["io"] = ioData;
 
-    //TODO PO INTAKE VALUES
 
-    //TODO CAPILLARY BLOOD GLUCOSE READING VALUES
 
-    //TODO SURVEY VALUES
+    //PO INTAKE DATA
+    let poData = {};
+
+    //MEAL CONSUMED VALUES
+    poData["meal-consumed-value"] = quickId("meal-consumed-value");
+    poData["meal-consumed-description"] = quickId("meal-consumed-description");
+
+    //ADD DATA TO "postValues"
+    postValues["po"] = poData;
+
+    //GLUCOSE DATA
+    let glucoseData = {};
+
+    //CAPILLARY BLOOD GLUCOSE READING VALUES
+    glucoseData["glucose-value"] = quickId("glucose-value");
+    glucoseData["glucose-description"] = quickId("glucose-description");
+
+    //ADD DATA TO "postValues"
+    postValues["glucose"] = glucoseData;
+
+
+
+    //SURVEY DATA
+    let surveyData = {};
+
+    //LOCAOS VALUES
+    surveyData["person-value"] = quickRadio("person-value");
+    surveyData["person-description"] = quickRadio("person-description");
+    surveyData["place-value"] = quickRadio("place-value");
+    surveyData["place-description"] = quickRadio("place-description");
+    surveyData["time-value"] = quickRadio("time-value");
+    surveyData["time-description"] = quickRadio("time-description");
+    surveyData["purpose-value"] = quickRadio("purpose-value");
+    surveyData["purpose-description"] = quickRadio("purpose-description");
+
+    //SKIN VALUES
+    surveyData["wbrl-value"] = quickRadio("wbrl-value");
+    surveyData["wbrl-description"] = quickRadio("wbrl-description");
+    surveyData["redness-irritation-value"] = quickRadio("redness-irritation-value");
+    surveyData["redness-irritation-description"] = quickRadio("redness-irritation-description");
+    surveyData["moisture-value"] = quickRadio("moisture-value");
+    surveyData["moisture-description"] = quickRadio("moisture-description");
+    surveyData["tct-value"] = quickRadio("tct-value");
+    surveyData["tct-description"] = quickRadio("tct-description");
+    surveyData["breakdown-value"] = quickRadio("breakdown-value");
+    surveyData["breakdown-description"] = quickRadio("breakdown-description");
+
+    //HEENT/NEURO VALUES
+    surveyData["head-value"] = quickRadio("head-value");
+    surveyData["head-description"] = quickRadio("head-description");
+    surveyData["eyes-vision-value"] = quickRadio("eyes-vision-value");
+    surveyData["eyes-vision-description"] = quickRadio("eyes-vision-description");
+    surveyData["ears-hearing-value"] = quickRadio("ears-hearing-value");
+    surveyData["ears-hearing-description"] = quickRadio("ears-hearing-description");
+    surveyData["nose-value"] = quickRadio("nose-value");
+    surveyData["nose-description"] = quickRadio("nose-description");
+    surveyData["throat-mouth-value"] = quickRadio("throat-mouth-value");
+    surveyData["throat-mouth-description"] = quickRadio("throat-mouth-description");
+
+    //LUNGS/THORATIC VALUES
+    surveyData["lung-sounds-value"] = quickRadio("lung-sounds-value");
+    surveyData["lung-sounds-description"] = quickRadio("lung-sounds-description");
+    surveyData["resp-depth-value"] = quickRadio("resp-depth-value");
+    surveyData["resp-depth-description"] = quickRadio("resp-depth-description");
+    surveyData["resp-rate-value"] = quickRadio("resp-rate-value");
+    surveyData["resp-rate-description"] = quickRadio("resp-rate-description");
+    surveyData["csa-value"] = quickRadio("csa-value");
+    surveyData["csa-description"] = quickRadio("csa-description");
+    surveyData["dlss-value"] = quickRadio("dlss-value");
+    surveyData["dlss-description"] = quickRadio("dlss-description");
+
+    //ABDOMEN/COCCYX/GENITALIA VALUES
+    surveyData["asa-value"] = quickRadio("asa-value");
+    surveyData["asa-description"] = quickRadio("asa-description");
+    surveyData["bowel-sounds-value"] = quickRadio("bowel-sounds-value");
+    surveyData["bowel-sounds-description"] = quickRadio("bowel-sounds-description");
+    surveyData["tlbd-value"] = quickRadio("tlbd-value");
+    surveyData["tlbd-description"] = quickRadio("tlbd-description");
+    surveyData["tdcs-value"] = quickRadio("tdcs-value");
+    surveyData["tdcs-description"] = quickRadio("tdcs-description");
+    surveyData["skin-appearance-value"] = quickRadio("skin-appearance-value");
+    surveyData["skin-appearance-description"] = quickRadio("skin-appearance-description");
+
+    //EXTREMITIES VALUES
+    surveyData["tccr-value"] = quickRadio("tccr-value");
+    surveyData["tccr-description"] = quickRadio("tccr-description");
+    surveyData["pul-sens-value"] = quickRadio("pul-sens-value");
+    surveyData["pul-sens-description"] = quickRadio("pul-sens-description");
+    surveyData["edema-value"] = quickRadio("edema-value");
+    surveyData["edema-description"] = quickRadio("edema-description");
+    surveyData["rom-value"] = quickRadio("rom-value");
+    surveyData["rom-description"] = quickRadio("rom-description");
+    surveyData["tdsscs-value"] = quickRadio("tdsscs-value");
+    surveyData["tdsscs-description"] = quickRadio("tdsscs-description");
+
+    //PSYCH/MENTAL HEALTH VALUES
+    surveyData["mood-affect-value"] = quickRadio("mood-affect-value");
+    surveyData["mood-affect-description"] = quickRadio("mood-affect-description");
+    surveyData["cognition-value"] = quickRadio("cognition-value");
+    surveyData["cognition-description"] = quickRadio("cognition-description");
+    surveyData["thou-patt-value"] = quickRadio("thou-patt-value");
+    surveyData["thou-patt-description"] = quickRadio("thou-patt-description");
+    surveyData["slee-patt-value"] = quickRadio("slee-patt-value");
+    surveyData["slee-patt-description"] = quickRadio("slee-patt-description");
+
+    //GENERAL VALUES
+    surveyData["pain-disc-value"] = quickRadio("pain-disc-value");
+    surveyData["pain-disc-description"] = quickRadio("pain-disc-description");
+    surveyData["elimination-value"] = quickRadio("elimination-value");
+    surveyData["elimination-description"] = quickRadio("elimination-description");
+    surveyData["appetite-value"] = quickRadio("appetite-value");
+    surveyData["appetite-description"] = quickRadio("appetite-description");
+    surveyData["gen-activity-value"] = quickRadio("gen-activity-value");
+    surveyData["gen-activity-description"] = quickRadio("gen-activity-description");
+
+    // ADD DATA TO "postValues"
+    postValues["survey"] = surveyData;
+
+    console.log(postValues);
+
 }
 
 //Takes id of HTML input element as parameter, returns value of element if not empty; if empty, it returns null
