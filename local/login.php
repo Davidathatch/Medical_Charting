3cc01e09d6128af497ff08692040056b866c7064
David Thatcher <54860843+Davidathatch@users.noreply.github.com>
2022-04-29 15:24:28 -0600

    Add files via upload
    Created login page, updated sidebar styles with subtle hover animation

---
+++ local/login.php
@@ -0,0 +1,38 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="formStyles.css">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="formStyles.css">
+
+    <title>Charting Login</title>
+</head>
+<body style="background-image: linear-gradient(to bottom right, #143058, #061126);">
+
+<div class="login-container">
+    <div class="login-content">
+        <div class="horizontal-group around">
+            <img src="../assets/catalyst-logo-blue.png" alt="Catalyst Logo">
+            <h1>Login</h1>
+        </div>
+        <div class="horizontal-group">
+            <input type="text" id="login-username" style="height: 50%; width: 100%" placeholder="Username">
+        </div>
+        <div class="horizontal-group button-end">
+            <input type="text" id="login-password" style="height: 50%;" placeholder="Password">
+            <button type="button" class="login-submit glance-button">Submit</button>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
